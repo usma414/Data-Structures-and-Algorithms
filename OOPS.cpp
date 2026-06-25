4da4beb876e6c68dc9978d3180f649ee4cd2f05a
usma414 <usmansarfraz2016@gmail.com>
2026-06-25 21:38:39 +0100

    Constructors done

--- OOPS.cpp
+++ OOPS.cpp
@@ -2,6 +2,55 @@
 #include <string>
 using namespace std;
 
+// class Teacher {
+
+//     // We use getters and setters for accessing private attributes and methods
+// private:           
+//     double salary;
+
+// public:
+//     string name;
+//     string subject;
+//     string dept;
+    
+//     void setSalary (int s) {
+//         salary = s;
+//     }
+
+//     double getSalary () {
+//         return salary;
+//     }
+
+//     // Method
+//     void changeDept (string newDept) {
+//         dept = newDept;
+//     }
+
+//     void teacherDetails () {
+//         cout << "Name: " << name << endl;
+//         cout << "Subject: " << subject << endl;
+//         cout << "Department: " << dept << endl;
+//         cout << "Salary: " << getSalary() << endl;
+//     }
+// };
+
+// int main () {
+
+//     Teacher t1;
+//     t1.name = "Humayun Sarfraz";
+//     t1.subject = "Physiology";
+//     t1.dept = "Medicine";
+//     t1.setSalary(46000);
+//     t1.getSalary();
+
+//     // t1.teacherDetails();
+
+//     t1.changeDept("MBBS");
+//     t1.teacherDetails();
+
+// }
+
+
 class Teacher {
 
     // We use getters and setters for accessing private attributes and methods
@@ -9,9 +58,17 @@ private:
     double salary;
 
 public:
+
     string name;
     string subject;
     string dept;
+
+    Teacher(string n, string sub, string d, double sal) {
+        name = n;
+        subject = sub;
+        dept = d;
+        salary = sal;
+    }
     
     void setSalary (int s) {
         salary = s;
@@ -36,17 +93,9 @@ public:
 
 int main () {
 
-    Teacher t1;
-    t1.name = "Humayun Sarfraz";
-    t1.subject = "Physiology";
-    t1.dept = "Medicine";
-    t1.setSalary(46000);
-    t1.getSalary();
-
-    // t1.teacherDetails();
-
-    t1.changeDept("MBBS");
-    t1.teacherDetails();
+   Teacher t1 = Teacher("Humayun Sarfraz", "Embryology", "Medicine", 29000);
+   Teacher t2 = Teacher("Maria Sarfraz", "Gastrology", "Surgery", 70000);
+   t1.teacherDetails();
+   t2.teacherDetails();
 
 }
-
