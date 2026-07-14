f5a90585633094c9fbbb8745e0601d661dabe54e
usma414 <usmansarfraz2016@gmail.com>
2026-07-14 23:48:17 +0100

    ProductExceptSelf revised

--- Leetcode_Problems/practiceProblems.cpp
+++ Leetcode_Problems/practiceProblems.cpp
@@ -34,6 +34,25 @@ public:
 
     
 
+    vector<int> productSelf(vector<int> nums){
+
+        int n = nums.size();
+        vector<int> ans(n,1);
+
+        for (int i = 1; i < n; i++ ) {
+            ans[i] = ans[i-1] * nums[i-1]; 
+        }
+
+        int suffix = 1;
+        for(int j = n - 2; j >=0; j--) {
+            suffix *= nums[j+1];
+            ans[j] *= suffix;
+        }
+
+        return ans;
+    }
+
+
 
 };
 
